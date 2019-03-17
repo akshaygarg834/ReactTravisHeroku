2c9d0766be24e1d26640ab0a7acd647da843e16c
Akshay <akshaygarg834@gmail.com>
2019-03-17 20:24:12 +0530

    JSON Placeholder

--- src/App.js
+++ src/App.js
@@ -3,23 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    }
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(data => {
+        return data.json()
+      })
+      .then(responsedata => this.setState({ users: responsedata }))
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React is awesome !!
-          </a>
+          <ul>
+            {this.state.users.map(user => <li key={user.id}>{user.name}</li>)}
+          </ul>
         </header>
+
       </div>
     );
   }
