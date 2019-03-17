fd1784d482ad523c94173113f9cb096d3cc89a97
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-17 14:56:46 +0000

    Deploy akshaygarg834/ReactTravisHeroku to github.com/akshaygarg834/ReactTravisHeroku.git:gh-pages

---
+++ precache-manifest.7513b655186f9a66798369ecfd6ec66d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab75505aa7ee5729f4db",
+    "url": "/ReactTravisHeroku/static/css/main.012967f4.chunk.css"
+  },
+  {
+    "revision": "ab75505aa7ee5729f4db",
+    "url": "/ReactTravisHeroku/static/js/main.48a38f99.chunk.js"
+  },
+  {
+    "revision": "e8712f69de0cb1690b69",
+    "url": "/ReactTravisHeroku/static/js/runtime~main.bfb12da8.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/ReactTravisHeroku/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactTravisHeroku/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7cd7bc65bdb7e0ff6e276ad7a8763237",
+    "url": "/ReactTravisHeroku/index.html"
+  }
+];
